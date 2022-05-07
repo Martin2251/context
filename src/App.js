b28b6ad634aa047edcf87e7393065951600494f0
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-05-07 13:15:51 +0100

    onto header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './container/Header';
+import Auth from './container/Auth';
+import {userContextProvider} from "./context/userContext";
 
 function App() {
   return (
     <div className="ui container">
-   
+      <userContextProvider >
+      <Header />
+      <Auth />
+      </userContextProvider>
+  
     </div>
   );
 }
